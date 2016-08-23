67adbf458c91c8ac33a2db5cdd1f3dbaa3243b17
Alexey Golubev <dr.freecx@gmail.com>
2016-08-23 12:13:49 +0300

    upd & add MAPE

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from itertools import product
 from collections import defaultdict
-from data import data_y, data_x
-from math import log
-import string
+from itertools import product
+from data import data_y
 import numpy as np
-
-alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+import string
 
 # find all occurrences `needle` in `haystack`
 def count_overlapping_substrings(haystack, needle):
@@ -17,72 +14,92 @@ def count_overlapping_substrings(haystack, needle):
             return count
         count += 1
 
-def represent_data(data, n_s=len(alphabet)):
-    if len(alphabet) < n_s:
-        raise('Length of alphabet is smaller that n_s')
-    ALPHA = alphabet[:n_s]
-    ALPHA_LEN = len(ALPHA) - 1
-    y_min, y_max = min(data_y), max(data_y)
-    y_delta = y_max - y_min
-    # normalize input data and present as symbol string
-    representer = lambda y: ALPHA[int(np.round(((y - y_min) / y_delta) * ALPHA_LEN))]
-    return ''.join(map(representer, data_y)), ALPHA
+class Representer:
+    PROB_MIN = 1E-300
+    ALPHABET = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+    ALPHABET_LEN = len(ALPHABET)
 
+    def __init__(self, data, n_w, n_s=ALPHABET_LEN):
+        if self.ALPHABET_LEN < n_s:
+            raise('Length of alphabet is smaller that n_s')
+        self.data = data
+        self.alphabet = self.ALPHABET[:n_s]
+        self.alpha_len = len(self.alphabet) - 1
+        self.result_string = self.represent(data)
+        self.calculate_frequence(n_w)
 
-def represent_values(string, data_y):
-    y_min, y_max = min(data_y), max(data_y)
-    for item in string:
-        yield (string.find(item) * y_max) / ALPHA_LEN + y_min
+    def __iadd__(self, new_data):
+        if type(new_data) not in (list, tuple):
+            raise('Type is not supported')
+        self.result_string += self.represent(new_data)
+        self.calculate_frequence(self.n_w)
 
+    def __isub__(self, substring):
+        if type(other) is not str:
+            raise('Type is not supported')
+        self.result_string.replace(substring, '')
+        self.calculate_frequence(self.n_w)
 
-def calc_freq(data_str, alphabet):
-    alphabet_dict = defaultdict(int)
-    result_freq = defaultdict(float)
-    dict_freq = defaultdict(float)
-    # calc p(a) ... p(z) freq
-    for item in data_str:
-        alphabet_dict[item] += 1
-    for k, v in alphabet_dict.items():
-        result_freq[k] = v / len(data_str)
-    # calc p(a|a), p(a|b), ..., p(b|a), ... freq
-    for a, b in product(alphabet, repeat=2):
-        # normalization parameter ( / len(data_str) )
-        count = count_overlapping_substrings(data_str, a + b) / len(data_str)
-        if count > 0:
-            dict_freq[a, b] = count
-    return result_freq, dict_freq
+    def represent(self, data):
+        self.data += data
+        self.dmin, self.dmax = min(self.data), max(self.data)
+        self.ddelta = self.dmax - self.dmin
+        self.dstep = self.ddelta / self.alpha_len
+        # normalize input data and present as symbol string
+        self.representer = lambda d: \
+            self.alphabet[int(np.round(((d - self.dmin) / self.ddelta) * self.alpha_len))]
+        return ''.join(map(self.representer, self.data))
 
-def classify(classifier, feats):
-    classes, prob = classifier
-    return min(classes.keys(),              # calculate argmin(-log(C|O))
-        key = lambda cl: -log(classes[cl]) + \
-            sum(-log(prob.get((cl, feat), 10**(-7))) for feat in feats))
+    def revert(self, string):
+        for item in string:
+            yield string.find(item) * self.dstep + self.dmin
+
+    def calculate_frequence(self, n_w, *, product_rep=2):
+        self.n_w = n_w
+        alphabet_dict = defaultdict(int)
+        self.result_freq = defaultdict(float)
+        self.dict_freq = defaultdict(float)
+        result_string_len = len(self.result_string)
+        # calc p(a) ... p(z) freq
+        for item in self.result_string:
+            alphabet_dict[item] += 1
+        for k, v in alphabet_dict.items():
+            self.result_freq[k] = v / result_string_len if v > 0 else self.PROB_MIN
+        # calc p(a|a), p(a|b), ..., p(b|a), ... freq
+        for item in product(self.alphabet, repeat=product_rep):
+            # normalization parameter ( result_string length )
+            freq = count_overlapping_substrings(self.result_string, ''.join(item)) / result_string_len 
+            self.dict_freq[item] = freq if freq > self.PROB_MIN else self.PROB_MIN
+        for start in range(0, result_string_len - n_w):
+            window = self.result_string[start:start + n_w]
+            for left in self.alphabet:
+                p = 1.0
+                # compute value p(left|window)
+                for right in window:
+                    p *= self.dict_freq[left, right]
+                self.dict_freq[left, window] = p if p > self.PROB_MIN else self.PROB_MIN
+
+    def classify(self, sample):
+        classes, prob = self.result_freq, self.dict_freq
+        return min(classes.keys(),              # calculate argmin(-log(C|O))
+            key = lambda cl: -np.log(classes[cl]) + \
+                sum(-np.log(prob.get((cl, feat), self.PROB_MIN)) for feat in sample))
 
 if __name__ == '__main__':
-    # select scanning window size
-    window_size = 5
-    res_str, alphabet = represent_data(data_y)
-    classifier = calc_freq(res_str, alphabet)
-    print('Represent string = `{}`'.format(res_str))
-    # compute freq for all scanning windows
-    # for start in range(0, len(res_str) - window_size):
-    #     window = res_str[start:start + window_size]
-    #     for left in ALPHA:
-    #         p = 1.0
-    #         # compute value p(left|window)
-    #         for right in window:
-    #             p *= comb_freq[left, right]
-    #         if p != 0:
-    #             comb_freq[left, window] = p
-    print('Classificate:')
-    for start in range(0, len(res_str) - window_size, 4):
-        window = res_str[start:start + window_size]
-        res = classify(classifier, window)
-        print('`{}` --> `{}`'.format(window, res))
-    freq_list = list(classifier[1].items())
-    freq_list.sort(key=lambda x: -x[1])
-    print('Frequency list sorted in descending order:')
-    max_field_size = int(np.round(np.log10(len(freq_list))))
-    for i, item in enumerate(freq_list[:20]):
-        if item[1] != 0:
-            print('{index:>{max}}. p({symb}) = {value:.4e}'.format(index=i, symb=item[0], value=item[1], max=max_field_size))
+    # our parameters
+    # n_s -- alphabet length
+    # n_w -- window size
+    n_s, n_w = 26, 5
+    rep = Representer(data_y, n_w, n_s)
+    res_len = len(rep.result_string)
+    for window_size in range(n_w, res_len - 1):
+        rep.calculate_frequence(window_size)
+        M_n = 0
+        n = 0
+        for start in range(0, res_len - window_size, window_size - 1):
+            window = rep.result_string[start:start + n_w]
+            key = tuple(rep.revert(rep.result_string[start + n_w] + rep.classify(window)))
+            M_n += abs((key[0] - key[1]) / key[0])
+            n += 1
+        M = M_n / n
+        print('window = {:4}; MAPE = {:.2f}%'.format(window_size, M * 100))
--- realization.py
+++ realization.py
@@ -1,13 +1,13 @@
 from main import count_overlapping_substrings
+from nltk import NaiveBayesClassifier, FreqDist
 from collections import defaultdict
 from itertools import product
 from data import data_y
 import numpy as np
 import string
-# import nltk
+import matplotlib.pyplot as plt
 
 class Representer:
-    PROB_MIN = 1E-300
     ALPHABET = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
     ALPHABET_LEN = len(ALPHABET)
 
@@ -26,12 +26,6 @@ class Representer:
         self.result_string += self.represent(new_data)
         self.calculate_frequence(self.n_w)
 
-    def __isub__(self, substring):
-        if type(other) is not str:
-            raise('Type is not supported')
-        self.result_string.replace(substring, '')
-        self.calculate_frequence(self.n_w)
-
     def represent(self, data):
         self.data += data
         self.dmin, self.dmax = min(self.data), max(self.data)
@@ -46,34 +40,60 @@ class Representer:
         for item in string:
             yield string.find(item) * self.dstep + self.dmin
 
-    def calculate_frequence(self, n_w, *, product_rep=2):
+    def _gen_feature(self, window, x):
+        result = defaultdict(int)
+        for item in window:
+            result[item] = self.labels[item]
+        return (result, x)
+
+    def calculate_frequence(self, n_w):
         self.n_w = n_w
-        alphabet_dict = defaultdict(int)
-        self.result_freq = defaultdict(float)
-        self.dict_freq = defaultdict(float)
+        self.labels = defaultdict(int)
         result_string_len = len(self.result_string)
-        # calc p(a) ... p(z) freq
-        for item in self.result_string:
-            alphabet_dict[item] += 1
-        for k, v in alphabet_dict.items():
-            self.result_freq[k] = v / result_string_len if v > 0 else self.PROB_MIN
-        # calc p(a|a), p(a|b), ..., p(b|a), ... freq
-        for a, b in product(self.alphabet, repeat=product_rep):
-            # normalization parameter ( result_string length )
-            freq = count_overlapping_substrings(self.result_string, a + b) / result_string_len 
-            self.dict_freq[a, b] = freq if freq > self.PROB_MIN else self.PROB_MIN
-        for start in range(0, result_string_len - n_w):
+        self.labels = FreqDist(self.result_string)
+        train = []
+        for start in range(0, len(self.result_string) - n_w, n_w - 1):
             window = self.result_string[start:start + n_w]
-            for left in self.alphabet:
-                p = 1.0
-                # compute value p(left|window)
-                for right in window:
-                    p *= self.dict_freq[left, right]
-                self.dict_freq[left, window] = p if p > self.PROB_MIN else self.PROB_MIN
+            x_key = self.result_string[start + n_w]
+            train.append(self._gen_feature(window, x_key))
+        self.classifier = NaiveBayesClassifier.train(train)
+
+    def classify(self, sample):
+        result = defaultdict(int)
+        for item in sample:
+            result[item] = self.labels[item]
+        return self.classifier.classify(result)
+
+def plot_bar(rep, name):
+    data, bins, bins2, labels = [[] for _ in range(4)]
+    for index, (label, item) in enumerate(sorted(rep.labels.items(), key=lambda x: x[0])):
+        data.append(rep.labels.freq(label))
+        labels.append(label)
+        bins.append(index)
+        bins2.append(index + 0.4)
+    ax = plt.subplot(111)
+    ax.bar(bins, data)
+    ax.set_xticks(bins2)
+    ax.set_xticklabels(labels)
+    ax.grid()
+    plt.xlim(min(bins), max(bins)+1)
+    plt.savefig(name)
 
 if __name__ == '__main__':
     # our parameters
     # n_s -- alphabet length
     # n_w -- window size
-    n_s, n_w = 26, 5
+    n_s, n_w = 26, 2
     rep = Representer(data_y, n_w, n_s)
+    res_len = len(rep.result_string)
+    for window_size in range(n_w, res_len - 1):
+        rep.calculate_frequence(window_size)
+        M_n = 0
+        n = 0
+        for start in range(0, res_len - window_size, window_size - 1):
+            window = rep.result_string[start:start + n_w]
+            key = tuple(rep.revert(rep.result_string[start + n_w] + rep.classify(window)))
+            M_n += abs((key[0] - key[1]) / key[0])
+            n += 1
+        M = M_n / n
+        print('window = {:4}; MAPE = {:.2f}%'.format(window_size, M * 100))
