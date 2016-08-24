8abf1fcc5830dd19832374866da7b9b3c7e2adfd
Alexey Golubev <dr.freecx@gmail.com>
2016-08-24 15:00:15 +0300

    [upd] working with ranges

--- realization.py
+++ realization.py
@@ -1,10 +1,13 @@
 from nltk import NaiveBayesClassifier, FreqDist
-from tools import mean_absolute_error, count_overlapping_substrings
+from itertools import product
 from collections import defaultdict
 from itertools import product
 from data import data
+import tools as tl
 import numpy as np
+import json as js
 import string
+# import sys
 
 class Representer:
     ALPHABET = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
@@ -68,26 +71,56 @@ class Representer:
             result[item] = self.labels[item]
         return self.classifier.classify(result)
 
+def select_train_data(data, *, window=96, feature_size=None):
+    train_data = None
+    remove_index = 0
+    sigma = 0
+    for index in range(0, len(data), window):
+        day = data[index:index + window]
+        curr_sigma = np.std(day)
+        if curr_sigma > sigma:
+            remove_index = index
+            train_data = day
+    data = data[0:index] + data[index + window:]
+    if feature_size:
+        return train_data, data[:feature_size]
+    else:
+        return train_data, data
+
 if __name__ == '__main__':
-    # our parameters
-    # n_s -- alphabet length
-    # n_w -- window size
-    n_s, n_w = 52, 20
+    # skip 31 days and take 1 train and 7 testing days
+    data = data[31 * 96:39 * 96]
     dmin, dmax = min(data), max(data)
-    train_data, test_data = data[:96], data[96:96 + 7 * 96]
-    classifier = Representer(n_w, n_s=n_s, dmin=dmin, dmax=dmax)
-    classifier.train(train_data)
-    test_repr = classifier.represent(test_data)
-    v_true, v_pred, l_true, l_pred = [[] for _ in range(4)]
-    for x in range(len(test_repr) - 1):
-        k_true = test_repr[x + 1]
-        k_pred = classifier.classify(test_repr[x])
-        key = tuple(classifier.revert(k_true + k_pred))
-        l_true.append(k_true)
-        l_pred.append(k_pred)
-        # for fix 'division by zero'
-        v_true.append(key[0] + 1.0)
-        v_pred.append(key[1] + 1.0)
-    print('alphabet = {:2}; window = {:2}; MAPE = {:6.2f}%; hits = {:6.2f}%'.format(
-        n_s, n_w, mean_absolute_error(v_true, v_pred), classifier.test(l_true, l_pred)
-    ))
+    train_data, test_data = select_train_data(data)
+    fp = open('data/nltk-result.csv', 'w')
+    # fp = sys.stdout
+    fp.write('alphabet;window;hits;MAPE;MAE;MSE;RMSE;ME;SD\n')
+    for n_s in range(8, Representer.ALPHABET_LEN // 3, 2):
+        for n_w in range(3, n_s // 2, 1):
+            classifier = Representer(n_w, n_s=n_s, dmin=dmin, dmax=dmax)
+            classifier.train(train_data)
+            test_repr = classifier.represent(test_data)
+            v_true, v_pred, l_true, l_pred = [[] for _ in range(4)]
+            for x in range(len(test_repr) - 1):
+                k_true = test_repr[x + 1]
+                k_pred = classifier.classify(test_repr[x])
+                key = tuple(classifier.revert(k_true + k_pred))
+                l_true.append(k_true)
+                l_pred.append(k_pred)
+                # for fix 'division by zero'
+                v_true.append(key[0] + 1.0)
+                v_pred.append(key[1] + 1.0)
+            with open('data/alph{:02}-wind{:03}.json'.format(n_s, n_w), 'w') as jsf:
+                raw_data = {
+                    'label_true': l_true,
+                    'label_pred': l_pred,
+                    'value_true': v_true,
+                    'value_pred': v_pred
+                }
+                js.dump(raw_data, jsf)
+            errors = tl.calculate_errors(v_true, v_pred)
+            fp.write('{};{};{:.2f}%;{:.2f}%;{:.2f};{:.2f};{:.2f};{:.2f};{:.2f}\n'.format(
+                n_s, n_w, classifier.test(l_true, l_pred), *errors
+            ).replace('.', ','))
+        tl.draw_bar(5, Representer.ALPHABET_LEN, n_s)
+    fp.close()
