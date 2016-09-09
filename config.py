752fd4fcd5d048b562506aa929c0d092de9f54ba
Alexey Golubev <dr.freecx@gmail.com>
2016-09-09 19:16:31 +0300

    [add] load csv file & plot graph

---
+++ config.py
@@ -0,0 +1,3 @@
+from os import path
+
+UPLOAD_FOLDER = path.join(path.dirname(__file__), 'upload')
--- webapp.py
+++ webapp.py
@@ -1,9 +1,37 @@
+from flask import render_template, jsonify, url_for, request, redirect
+from werkzeug import secure_filename
+from csv import DictReader
 from __init__ import app
-from flask import render_template
+from os import path
 
-@app.route('/')
+@app.route('/analyze', methods=['GET', 'POST'])
+def analyze(file=None):
+    if request.method == 'GET':
+        return render_template('analyze.html', file=request.args['file'])
+    elif request.method == 'POST':
+        folder = app.config['UPLOAD_FOLDER']
+        file = path.join(folder, secure_filename(request.args['file']))
+        if path.exists(file):
+            raw = list(DictReader(open(file, 'r'), delimiter=';'))
+            window = 96 * 7
+            data = list(map(
+                lambda x: [x[0], float(x[1]['Energy'].replace(',', '.'))], 
+                enumerate(raw[:window])))
+            return jsonify({'data': [['x', 'y']] + data})
+        return 'File not found', 404
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        up_file = request.files['file']
+        if up_file:
+            folder = app.config['UPLOAD_FOLDER']
+            filename = path.join(folder, secure_filename(up_file.filename))
+            up_file.save(filename)
+            return redirect(url_for('analyze', file=up_file.filename))
+        return 'Something happened', 400
 
 if __name__ == '__main__':
     app.run(debug=True)
