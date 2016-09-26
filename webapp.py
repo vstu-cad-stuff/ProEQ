949beb3b574b9f3308b28886c04ed76240a015b5
Alexey Golubev <dr.freecx@gmail.com>
2016-09-26 13:57:41 +0300

    [upd] move to bootstrap (WIP)

--- webapp.py
+++ webapp.py
@@ -1,22 +1,26 @@
+from flask_user import login_required, SQLAlchemyAdapter, UserManager, current_user
 from flask import render_template, jsonify, url_for, request, redirect
-from flask_user import login_required, SQLAlchemyAdapter, UserManager
 from werkzeug import secure_filename
 from __init__ import app, db
 from csv import DictReader
 from model import User
 from os import path
 
-
-@app.route('/analyze', methods=['GET', 'POST'])
+@app.route('/dashboard')
 @login_required
-def analyze(file=None):
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/dashboard/analyze', methods=['GET', 'POST'])
+@login_required
+def dashboard_analyze(file=None):
     if request.method == 'GET':
         return render_template('analyze.html', file=request.args['file'])
     elif request.method == 'POST':
         folder = app.config['UPLOAD_FOLDER']
-        file = path.join(folder, secure_filename(request.args['file']))
-        if path.exists(file):
-            raw = list(DictReader(open(file, 'r'), delimiter=';'))
+        filename = path.join(folder, secure_filename(request.args['file']))
+        if path.exists(filename):
+            raw = list(DictReader(open(filename, 'r'), delimiter=';'))
             window = 96 * 7
             data = list(map(
                 lambda x: [x[0], float(x[1]['Energy'].replace(',', '.'))], 
@@ -24,14 +28,9 @@ def analyze(file=None):
             return jsonify({'data': [['x', 'y']] + data})
         return 'File not found', 404
 
-@app.route('/dashboard')
+@app.route('/dashboard/load', methods=['GET', 'POST'])
 @login_required
-def dashboard():
-    return render_template('dashboard.html')
-
-@app.route('/load', methods=['GET', 'POST'])
-@login_required
-def load():
+def dashboard_load():
     if request.method == 'GET':
         return render_template('load.html')
     elif request.method == 'POST':
@@ -40,8 +39,7 @@ def load():
             folder = app.config['UPLOAD_FOLDER']
             filename = path.join(folder, secure_filename(up_file.filename))
             up_file.save(filename)
-            return redirect(url_for('analyze', file=up_file.filename))
-        return 'Something happened', 400
+            return redirect(url_for('dashboard_analyze', file=up_file.filename))
 
 @app.route('/')
 def index():
