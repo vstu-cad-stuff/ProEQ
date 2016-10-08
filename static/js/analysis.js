85a827fa176a1efe9b3520df8ddd152a1aad55c6
Alexey Golubev <dr.freecx@gmail.com>
2016-10-08 16:11:50 +0300

    form redesign and send data
    - update form design
    - add send data function
    - remove unused pages
    - and small fixes

--- static/js/analysis.js
+++ static/js/analysis.js
@@ -1,15 +1,22 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
+function gId(name) {
+  return document.getElementById(name);
+}
+
 function drawChart() {
   var jsonData = JSON.parse($.ajax({
-    url: "/dashboard/analysis?file=" + filename_url,
+    url: '/dashboard/analysis?file=' + filename_url,
     dataType: 'json',
     method: 'post',
     async: false
-  }).responseText).data;
+  }).responseText);
 
-  var data = new google.visualization.arrayToDataTable(jsonData);
+  gId('point_count').innerHTML = jsonData.count;
+  gId('point_period').innerHTML = jsonData.period;
+  gId('point_range').innerHTML = '[' + jsonData.range[0] + ', ' + jsonData.range[1] + ']';
+  var data = new google.visualization.arrayToDataTable(jsonData.data);
 
   var options = {
     title: 'Загруженные данные',
@@ -20,4 +27,20 @@ function drawChart() {
   var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 
   chart.draw(data, options);
+}
+
+function SendData() {
+  send_data = {
+    alphabet_len:   parseInt(gId('alphabet_len').value),
+    window_len:     parseInt(gId('window_len').value),
+    train_range:    [parseInt(gId('train_start').value), parseInt(gId('train_end').value)],
+    forecast_range: [parseInt(gId('forecast_start').value), parseInt(gId('forecast_end').value)]
+  };
+  $.ajax({
+    contentType: 'application/json',
+    url: '/dashboard/model',
+    dataType: 'json',
+    type: 'POST',
+    data: JSON.stringify(send_data)
+  });
 }
--- webapp.py
+++ webapp.py
@@ -2,9 +2,13 @@ from flask_user import login_required, SQLAlchemyAdapter, UserManager, current_u
 from flask import render_template, jsonify, url_for, request, redirect
 from werkzeug import secure_filename
 from __init__ import app, db
+from os import path, listdir
 from csv import DictReader
 from model import User
-from os import path
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/dashboard')
 @login_required
@@ -21,18 +25,25 @@ def dashboard_analysis(file=None):
         filename = path.join(folder, secure_filename(request.args['file']))
         if path.exists(filename):
             raw = list(DictReader(open(filename, 'r'), delimiter=';'))
-            window = 96 * 7
+            window_start, window_end = (0, 96 * 7)
             data = list(map(
                 lambda x: [x[0], float(x[1]['Energy'].replace(',', '.'))], 
-                enumerate(raw[:window])))
-            return jsonify({'data': [['x', 'y']] + data})
+                enumerate(raw[window_start:window_end])))
+            return_data = {
+                'data': [['x', 'y']] + data,
+                'count': len(raw),
+                'range': [window_start, window_end],
+                'period': 96, # calculate it!
+            }
+            return jsonify(return_data)
         return 'File not found', 404
 
-@app.route('/dashboard/load', methods=['GET', 'POST'])
+@app.route('/dashboard/data', methods=['GET', 'POST'])
 @login_required
-def dashboard_load():
+def dashboard_data():
     if request.method == 'GET':
-        return render_template('load.html')
+        file_list = listdir(app.config['UPLOAD_FOLDER'])
+        return render_template('data.html', args={'file_list': file_list})
     elif request.method == 'POST':
         up_file = request.files.get('file')
         if up_file:
@@ -41,9 +52,13 @@ def dashboard_load():
             up_file.save(filename)
             return redirect(url_for('dashboard_analysis', file=up_file.filename))
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/dashboard/model', methods=['POST'])
+@login_required
+def model():
+    params = request.get_json()
+    # write code for predict model
+    # and send result to web-gui
+    return 'Okay :)', 200
 
 if __name__ == '__main__':
     db_adapter = SQLAlchemyAdapter(db, User)
