ec3e3bb8278fcc3506e751bc6375607b7ff0878e
Alexey Golubev <dr.freecx@gmail.com>
2016-10-11 14:57:06 +0300

    [clean] remove unused code

--- web/create_demo.py
+++ web/create_demo.py
@@ -1,9 +1,6 @@
-from flask_user import SQLAlchemyAdapter, UserManager
-from model import db, app, User, Role
+from model import db, app, User, Role, user_manager
 
 if __name__ == '__main__':
-    db_adapter = SQLAlchemyAdapter(db, User)
-    user_manager = UserManager(db_adapter, app)
     db.create_all()
     demo = User(username='demo', password=user_manager.hash_password('demo'), active=True)
     role = Role(name='user')
--- web/webapp.py
+++ web/webapp.py
@@ -77,4 +77,4 @@ def model():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
